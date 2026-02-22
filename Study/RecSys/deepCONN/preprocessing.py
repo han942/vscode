982a13ea50952a0f830043c0c44e044a12843adb
han942 <hananthony1@gmail.com>
2026-02-22 23:35:59 +0900

    Implement data loading function with train-test split and pivot table creation

--- Study/RecSys/deepCONN/preprocessing.py
+++ Study/RecSys/deepCONN/preprocessing.py
@@ -1,7 +1,33 @@
 import pandas as pd
 import numpy as np
 from config import Config
+from collections import Counter
+from konlpy.tag import Okt
+from tqdm import tqdm
+from pathlib import Path
 
-config = Config()
+def load_data(base,test,data_indicator=0):
+    if data_indicator==0:
+        path = Path('../../../datafile/Recsys/ml-100k')
+        train = pd.read_csv(path / base,sep='\t',
+                    usecols=[0,1,2],names=['user','item','rating'],
+                    dtype={'user':int,'item':int,'rating':float})
+        test = pd.read_csv(path / test,sep='\t',
+                    usecols=[0,1,2],names=['user','item','rating'],
+                    dtype={'user':int,'item':int,'rating':float})
 
-df = 
+        R_train = pd.pivot_table(train,values='rating',index='user',columns='item')
+    elif data_indicator==1:
+        path = Path('../../../datafile/Recsys/ml-100k')
+        data = pd.read_csv(path / 'u.data',sep='\t',
+                    usecols=[0,1,2],names=['user','item','rating'],
+                    dtype={'user':int,'item':int,'rating':float})
+
+        train = data.groupby('user').sample(frac=0.8,random_state=42)
+        train_ind = train.index
+        test = data.drop(train_ind)
+
+        print(f'Data Size //  original: {data.shape}, train:{train.shape}, test:{test.shape}')
+        R_train = pd.pivot_table(train,values='rating',index='user',columns='item')
+
+    return train,test,R_train
